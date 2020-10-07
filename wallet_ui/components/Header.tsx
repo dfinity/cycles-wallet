01cfe44c2e6143e96f79e20eca6bbcedd3e76142
Hans Larsen <hans@larsen.online>
2020-10-07 00:02:15 -0700

    add header canister id and fix build system

--- wallet_ui/components/Header.tsx
+++ wallet_ui/components/Header.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import { getWalletPrincipal } from "../api";
 import '../css/Header.css';
 const logo = require('../img/logo.png').default;
 
 export default () => (
-  <img className="logo" alt="DFINITY Logo" src={logo} />
+  <div>
+    <span className="wallet-id">{getWalletPrincipal()}</span>
+    <img className="logo" alt="DFINITY Logo" src={logo} />
+  </div>
 );
