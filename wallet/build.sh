b65814234b753f03df3947f91aa94fa69606fc26
Prithvi Shahi <prithvi@dfinity.org>
2020-12-06 15:40:43 -0800

    revert changes to dfx.json, demo.sh, and wallet/build.sh

--- wallet/build.sh
+++ wallet/build.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
-set -e
 
-#npm run build
-
-# Delete the WASM to make sure we're taking the new bytes in.
-cargo build --manifest-path $(dirname $0)/Cargo.toml --target wasm32-unknown-unknown --release
+cargo build --manifest-path $(dirname $0)/Cargo.toml --target wasm32-unknown-unknown --release || exit 1
 
 CURRENT_DIR=`pwd`
 cd $(dirname $0)/
 cargo install ic-cdk-optimizer --root target || exit 1
+STATUS=$?
 cd $CURRENT_DIR
 
-"$(dirname $0)/target/bin/ic-cdk-optimizer" \
-    "$(dirname $0)/target/wasm32-unknown-unknown/release/wallet.wasm" \
-    -o "$(dirname $0)/target/wasm32-unknown-unknown/release/wallet-$1.wasm"
+if [ "$STATUS" -eq "0" ]; then
+  $(dirname $0)/target/bin/ic-cdk-optimizer \
+      $(dirname $0)/target/wasm32-unknown-unknown/release/wallet.wasm \
+      -o $(dirname $0)/target/wasm32-unknown-unknown/release/wallet-opt.wasm
+
+  true
+else
+  false
+fi
