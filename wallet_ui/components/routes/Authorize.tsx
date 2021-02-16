d909e55fa38ce0c4092dab2e7412a4e47a8396fc
Andrew Wylde <andrew.wylde@dfinity.org>
2021-02-16 10:20:20 -0800

    fix conflicts

--- wallet_ui/components/routes/Authorize.tsx
+++ wallet_ui/components/routes/Authorize.tsx
@@ -71,7 +71,7 @@ export function Authorize({ dark }: { dark: boolean }) {
   useEffect(() => {
     getAgentPrincipal().then(p => {
       log('debug', 'getPrincipal() got p, passing it to setPrincipal now', p)
-      setPrincipal(p);
+      setAgentPrincipal(p);
     });
 
     checkAccess();
