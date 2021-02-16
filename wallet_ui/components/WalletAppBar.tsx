ec810c874a869011a135004ae1bcb75b6c5f1b37
Andrew Wylde <andrew.wylde@dfinity.org>
2021-02-15 17:18:04 -0800

    refactor(ui): put wallet id at top right of app bar
    also gets the walletId

--- wallet_ui/components/WalletAppBar.tsx
+++ wallet_ui/components/WalletAppBar.tsx
@@ -1,13 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { useRouteMatch } from "react-router-dom";
 import clsx from "clsx";
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
 import MenuIcon from "@material-ui/icons/Menu";
-import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
 import Switch from "@material-ui/core/Switch";
-import React from "react";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
 import { makeStyles } from "@material-ui/core/styles";
-import { useRouteMatch } from "react-router-dom";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+
+import { getWalletId, Principal } from "../canister";
 
 // Assets
 const logo = require("../img/logo.png").default;
@@ -31,7 +38,8 @@ const useStyles = makeStyles((theme) => ({
     }),
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: "auto",
+    justifyContent: "start",
   },
   menuButtonHidden: {
     display: "none",
@@ -40,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: 24, // keep right padding when drawer closed
   },
   title: {
-    flexGrow: 1,
+    marginLeft: "auto",
   },
   logo: {
     height: 16,
@@ -55,39 +63,72 @@ export function WalletAppBar(props: {
   onDarkToggle: () => void;
 }) {
   const { dark, open, onOpenToggle, onDarkToggle } = props;
+  const [walletId, setWalletId] = useState<Principal | null>();
+  useEffect(() => {
+    const walletId = getWalletId(null);
+    if (walletId === null) {
+      return;
+    }
+    setWalletId(walletId);
+  }, []);
   const classes = useStyles();
   const menu = !useRouteMatch("/authorize");
 
-  return (
-    <AppBar
-      position="absolute"
-      className={clsx(classes.appBar, open && menu && classes.appBarShift)}
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = "primary-search-account-menu";
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
     >
+      <MenuItem onClick={handleMenuClose}>
+        <FormControlLabel control={<Switch checked={dark} onChange={onDarkToggle} />} label="Dark Mode" />
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <AppBar position="absolute" className={clsx(classes.appBar, open && menu && classes.appBarShift)}>
       <Toolbar className={classes.toolbar}>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          onClick={onOpenToggle}
-          className={clsx(
-            classes.menuButton,
-            (open || !menu) && classes.menuButtonHidden
-          )}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography
-          component="h1"
-          variant="h6"
-          color="inherit"
-          noWrap
-          className={classes.title}
-        >
-          <img alt="DFINITY Logo" src={logo} className={classes.logo} />
-          Wallet
-        </Typography>
-        <Switch checked={dark} onChange={onDarkToggle} />
+        <MenuItem className={clsx(classes.menuButton, (open || !menu) && classes.menuButtonHidden)}>
+          <IconButton edge="start" color="inherit" aria-label="open drawer" onClick={onOpenToggle}>
+            <MenuIcon />
+          </IconButton>
+        </MenuItem>
+        <MenuItem className={classes.title}>
+          <Typography variant="h6" color="inherit" noWrap className={classes.title}>
+            {walletId && walletId.toText()}
+          </Typography>
+        </MenuItem>
+        <MenuItem onClick={handleProfileMenuOpen}>
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+        </MenuItem>
       </Toolbar>
+      {renderMenu}
     </AppBar>
   );
 }
