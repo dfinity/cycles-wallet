9256ef65170d58a43fb4055433f64dcefd11f5a7
Prithvi Shahi <prithvi.shahi@dfinity.org>
2021-03-17 14:40:32 -0700

    fix: remove default memory allocation

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -217,8 +217,6 @@ mod wallet {
     use ic_cdk_macros::*;
     use serde::Deserialize;
 
-    const DEFAULT_MEM_ALLOCATION: u64 = 40000000_u64; // 40 MB
-
     /***************************************************************************************************
      * Cycle Management
      **************************************************************************************************/
@@ -425,7 +423,7 @@ mod wallet {
             wasm_module: wasm_module.clone(),
             arg: b" ".to_vec(),
             compute_allocation: None,
-            memory_allocation: Some(Nat::from(DEFAULT_MEM_ALLOCATION)),
+            memory_allocation: None,
         };
 
         match api::call::call(
