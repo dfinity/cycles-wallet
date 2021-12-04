0f4e6c9f243f2a19caef7f7c552c9adc6ad43140
adamspofford-dfinity <93943719+adamspofford-dfinity@users.noreply.github.com>
2021-12-03 17:47:30 -0800

    feat: Set both caller and self as controller by default (#110)

--- e2e/bash/controller.bash
+++ e2e/bash/controller.bash
@@ -29,15 +29,16 @@ teardown() {
     assert_command dfx identity use alice
 
     ALICE_WALLET=$(dfx --identity alice identity get-wallet)
+    ALICE_ID=$(dfx --identity alice identity get-principal)
 
     dfx --identity alice canister create --all
 
     assert_command dfx --identity alice canister status e2e_project
-    assert_match "Controller: $ALICE_WALLET"
+    assert_match "Controllers: ($ALICE_WALLET $ALICE_ID|$ALICE_ID $ALICE_WALLET)"
     assert_match "Module hash: None"
 
     assert_command dfx --identity alice canister info e2e_project
-    assert_match "Controller: $ALICE_WALLET"
+    assert_match "Controllers: ($ALICE_WALLET $ALICE_ID|$ALICE_ID $ALICE_WALLET)"
     assert_match "Module hash: None"
 
     dfx --identity alice canister status e2e_project
@@ -52,17 +53,18 @@ teardown() {
     assert_command dfx identity use alice
 
     ALICE_WALLET=$(dfx --identity alice identity get-wallet)
+    ALICE_ID=$(dfx --identity alice identity get-principal)
 
     dfx --identity alice canister create --all
     dfx --identity alice build
     dfx --identity alice canister install --all
 
     assert_command dfx --identity alice canister status e2e_project
-    assert_match "Controller: $ALICE_WALLET"
+    assert_match "Controllers: ($ALICE_WALLET $ALICE_ID|$ALICE_ID $ALICE_WALLET)"
     assert_match "Module hash: 0x"
 
     assert_command dfx --identity alice canister info e2e_project
-    assert_match "Controller: $ALICE_WALLET"
+    assert_match "Controllers: ($ALICE_WALLET $ALICE_ID|$ALICE_ID $ALICE_WALLET)"
     assert_match "Module hash: 0x"
 }
 
@@ -76,28 +78,30 @@ teardown() {
     assert_command dfx identity use alice
 
     ALICE_WALLET=$(dfx --identity alice identity get-wallet)
+    ALICE_ID=$(dfx --identity alice identity get-principal)
     BOB_WALLET=$(dfx --identity bob identity get-wallet)
+    BOB_ID=$(dfx --identity bob identity get-principal)
 
     dfx deploy e2e_project
 
     assert_command dfx --identity alice canister status e2e_project
-    assert_match "Controller: $ALICE_WALLET"
+    assert_match "Controllers: ($ALICE_WALLET $ALICE_ID|$ALICE_ID $ALICE_WALLET)"
 
     # Set controller using canister name and identity name
     # Surprise: This doesn't actually call update_settings in the wallet
     # canister.  It calls wallet_call on the wallet canister, which
     # forwards to update_settings on the management canister.
     assert_command dfx canister update-settings e2e_project --controller "${BOB_WALLET}"
-    assert_match "Updated \"${BOB_WALLET}\" as controller of \"e2e_project\"."
+    assert_match "Set controller of \"e2e_project\" to: ${BOB_WALLET}"
 
     assert_command dfx --identity bob canister status e2e_project
-    assert_match "Controller: $BOB_WALLET"
+    assert_match "Controllers: $BOB_WALLET"
 
     # Bob is controller, Alice cannot reinstall
-    assert_command_fail dfx canister install e2e_project -m reinstall
+    echo yes | assert_command_fail dfx canister install e2e_project -m reinstall
 
     # Bob can reinstall
-    assert_command dfx --identity bob canister install e2e_project -m reinstall
+    echo yes | assert_command dfx --identity bob canister install e2e_project -m reinstall
 }
 
 @test "create wallet with single controller through wallet_create_wallet" {
